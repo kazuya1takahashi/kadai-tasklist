f8da7167beb129391ee0d5b0f983445c88b4f7d6
kazuya.takahashi <kazuya.cloud9@gmail.com>
2017-08-24 15:13:09 +0000

    user login

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 20170817133114) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "status"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -27,4 +29,5 @@ ActiveRecord::Schema.define(version: 20170817133114) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
